96608783254fa02ceb1672c6d22b57f739900a32
Rafael Crempe <rafaelcrempe@gmail.com>
2026-04-21 21:43:47 -0300

    feat(backend/user): criação de usuário com validação e criptografia de senha

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,9 +1,9 @@
-import { FastifyReply, FastifyRequest } from 'fastify';
+import { FastifyReply, FastifyRequest } from "fastify";
+import { prisma } from "../lib/prisma";
+import bcrypt from "bcrypt";
 
-export async function createUser(
-  request: FastifyRequest,
-  reply: FastifyReply
-) {
+
+export async function createUser(request: FastifyRequest, reply: FastifyReply) {
   const body = request.body as {
     name: string;
     lastName: string;
@@ -12,8 +12,44 @@ export async function createUser(
     password: string;
   };
 
-  return reply.send({
-    message: 'Controller funcionando',
-    received: body,
+  const { name, lastName, username, email, password } = body;
+
+  const existingUsername = await prisma.user.findUnique({
+    where: { username },
+  });
+
+  if (existingUsername) {
+    return reply.status(409).send({
+      error: "Conflict",
+      message: "Nome de Usuário já cadastrado.",
+    });
+  }
+
+  const existingEmail = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (existingEmail) {
+    return reply.status(409).send({
+      error: "Conflict",
+      message: "E-mail já registrado.",
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await prisma.user.create({
+      data: {
+        name,
+        lastName,
+        password: hashedPassword,
+        username,
+        email
+      },
+    });
+
+  return reply.status(201).send({
+    message: "Usuário criado com sucesso!",
+    data:user,
   });
 }
